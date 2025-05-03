a22c3b426f1e1138822d0c77507894af73d340b9
Jesus Rosales <jesusrosales07537@gmail.com>
2025-05-02 21:11:21 -0600

    comenzando con login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,24 +6,30 @@ const page = () => {
   return (
     // grid [40, 60] for left and right sections, ajustado para móviles
     <main className="max-w-[2000px] mx-auto grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-[45%_55%] h-screen">
-      
       {/* left section */}
-      <div className="flex my-4 xl:my-3 flex-col h-full w-full px-4 sm:px-6 sm:w-5/6 md:w-5/6 2xl:w-4/6 mx-auto lg:gap-8">
+      <div className="flex my-4 xl:my-3 flex-col h-full w-full px-4 sm:px-6 sm:w-5/6 md:w-5/6 2xl:w-4/6 mx-auto ">
         {/* Header fixed at top */}
         <header className="">
           <p className="text-xl sm:text-2xl flex items-center font-medium">
-            <span className="text-white font-bold rounded-full px-2 sm:px-3 text-2xl sm:text-3xl bg-blue-900">E</span>commerce
+            <span className="text-white font-bold rounded-full px-2 sm:px-3 text-2xl sm:text-3xl bg-blue-900">
+              E
+            </span>
+            commerce
           </p>
         </header>
 
         {/* Content and form centered */}
         <div className="flex flex-col grow justify-center px-2 sm:px-4 w-full">
-          <div className="mb-2 sm:mb-3">
-            <h1 className="text-2xl xl:text-3xl 2xl:text-4xl sm:text-4xl  font-bold mb-2 sm:mb-3">Bienvenido de nuevo</h1>
-            <p className="text-sm sm:text-base text-gray-600 mb-3 sm:mb-4">
-              Inicia sesión para acceder a tu cuenta y disfrutar de todas las
-              funciones.
-            </p> 
+          <div className="mb-2 sm:mb-3 lg:mb-4">
+            <h1 className="text-2xl xl:text-3xl 2xl:text-4xl sm:text-4xl  font-bold mb-2 sm:mb-4">
+              Bienvenido de nuevo
+            </h1>
+            <p className="text-base sm:text-base lg:text-base text-gray-600">
+              ¿No tienes una cuenta?{" "}
+              <a href="#" className="text-blue-600 hover:underline">
+                Crea una aqui
+              </a>
+            </p>
           </div>
 
           <LoginForm />
--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -14,7 +14,8 @@ import {
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { IoLogoGoogle } from "react-icons/io";
+import { IoLogoGoogle, IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { useState } from "react";
 
 const formSchema = z.object({
   email: z.string().email({
@@ -26,6 +27,8 @@ const formSchema = z.object({
 });
 
 export function LoginForm() {
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -40,15 +43,24 @@ export function LoginForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 sm:space-y-8 w-full">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-6 sm:space-y-8 w-full"
+      >
         <FormField
           control={form.control}
           name="email"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel className="text-base md:text-lg text-gray-600">Correo:</FormLabel>
+            <FormItem className="mb-4">
+              <FormLabel className="text-base md:text-lg text-gray-600">
+                Correo:
+              </FormLabel>
               <FormControl>
-                <Input className="h-10 sm:h-11" placeholder="shadcn" {...field} />
+                <Input
+                  className="h-10 sm:h-11"
+                  placeholder="ejemplo@gmail.com"
+                  {...field}
+                />
               </FormControl>
 
               <FormMessage />
@@ -60,38 +72,65 @@ export function LoginForm() {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel className="text-base md:text-lg text-gray-600">Password</FormLabel>
-              <FormControl>
-                <Input
-                  className="h-10 sm:h-11"
-                  type="password"
-                  placeholder="********"
-                  {...field}
-                />
-              </FormControl>
+            <FormItem className="mb-4">
+              <FormLabel className="text-base md:text-lg text-gray-600">
+                Password
+              </FormLabel>
 
+              {/* input con boton icono  */}
+              <div className="flex items-center">
+                <FormControl>
+                  <Input
+                    type={passwordVisible ? "text" : "password"}
+                    className="h-10 sm:h-11 pr-10 rounded-e-none"
+                    placeholder="********"
+                    {...field}
+                  />
+                </FormControl>
+                <Button
+                onClick={() => setPasswordVisible(!passwordVisible)}
+                  type="button"
+                  variant={"ghost"}
+                  className="w-10  rounded-s-none border h-full inset-y-0 right-0 flex items-center pr-3 text-gray-500 "
+                >
+                  {!passwordVisible ? (
+                    <IoMdEye size={20} />
+                  ) : (
+                    <IoMdEyeOff size={20} />
+                  )}
+                </Button>
+              </div>
               <FormMessage />
             </FormItem>
           )}
         />
         {/* olvide password */}
-        <div className="flex flex-col sm:flex-row sm:justify-between items-start sm:items-center gap-2 sm:gap-0 mb-2 sm:mb-4">
-          <p className="text-base sm:text-base lg:text-xs text-gray-600">
-            ¿No tienes una cuenta? <a href="#" className="text-blue-600 hover:underline">Click aqui</a>
-          </p>
+        <div className="flex flex-row justify-between items-center gap-2 sm:gap-0 mb-4 sm:mb-4">
+          <div className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              id="remember"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+            />
+            <label
+              htmlFor="remember"
+              className="text-sm sm:text-base text-gray-600"
+            >
+              Recordar contraseña
+            </label>
+          </div>
           <a
             href="#"
-            className="text-xs sm:text-sm lg:text-xs  font-semibold text-gray-600 hover:underline"
+            className="text-xs sm:text-sm   font-semibold text-gray-600 hover:underline"
           >
             Olvidé mi contraseña
           </a>
         </div>
-        <Button type="submit" className="w-full h-10 sm:h-11 bg-blue-900">
+        <Button type="submit" className="w-full h-10 sm:h-11 bg-blue-900 mb-4 ">
           Submit
         </Button>
 
-        <div className="flex items-center justify-between mt-3 sm:mt-4">
+        <div className="flex items-center justify-between mb-4">
           <div className="h-px bg-gray-300 flex-grow mr-2"></div>
           <span className="text-gray-500 text-xs sm:text-sm">O</span>
           <div className="h-px bg-gray-300 flex-grow ml-2"></div>
@@ -105,7 +144,7 @@ export function LoginForm() {
             <IoLogoGoogle className="mr-2" size={18} />
             Iniciar sesión con Google
           </Button>
-          </div>
+        </div>
       </form>
     </Form>
   );
