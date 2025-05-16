6be7a567fad02c16ec5836f09f4b097ccf5b9829
Jesus Rosales <jesusrosales07537@gmail.com>
2025-05-15 18:00:07 -0600

    feat(products): Funcionalidad de siguiente/atras en crear usuario

--- src/features/products/components/WizardContent.tsx
+++ src/features/products/components/WizardContent.tsx
@@ -1,18 +1,33 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useWizardStore } from "../store/useWizardStore";
+import { ProductDescriptionForm } from "./ProductDescriptionForm";
+import { ProductGeneralForm } from "./ProductGeneralForm";
+import { ProductImagesForm } from "./ProductImagesForm";
+import { ProductSpecificationsForm } from "./ProductSpecificationsForm";
 
 
 export const WizardContent = () => {
-    const {activeStep, nextStep} = useWizardStore();
+    const {activeStep, nextStep, prevStep, steps} = useWizardStore();
 
   return (
     <div>
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <h2 className="text-2xl font-bold">Paso {activeStep + 1}</h2>
-            <p className="text-sm text-muted-foreground">
-            Contenido del paso {activeStep + 1}
-            </p>
+        {activeStep === 0 && <ProductGeneralForm />}
+        {activeStep === 1 && <ProductSpecificationsForm />}
+        {activeStep === 2 && <ProductDescriptionForm />}
+        {activeStep === 3 && <ProductImagesForm />}
+
+
+        <div className="flex justify-between itecms-center ">
+        <Button type="button" variant="outline" onClick={() => prevStep()}>
+            Regresar
+        </Button>
+        <Button type="submit" onClick={() => nextStep()} >
+            {
+                steps.length - 1 === activeStep ? "Guardar" : "Siguiente"
+            }
+        </Button>
         </div>
     </div>
   )
